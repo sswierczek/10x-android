e35187b4b8f743f33331c0d054133f6f08b1e3fc
sswierczek <sebastian.swierczek@gmail.com>
2025-04-13 13:23:00 +0200

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,22 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        verbose.set(true)
+        outputToConsole.set(true)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+        filter {
+            exclude { element -> element.file.path.contains("build/") }
+        }
+    }
 }
